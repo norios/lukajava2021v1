77d192dbe4cb72db87949786d4c3a46a55385bf3
norios <norio_suzuki@hotmail.com>
2021-05-22 14:19:14 +0900

    1st

---
+++ k0705.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class k0705 {
+    public static void main(String[] args) throws Exception {
+        Scanner stdIn = new Scanner(System.in);  
+        int n =1;
+        int i = 1;
+        int nprev = 0;
+
+        while( n!=0 ){
+            n = stdIn.nextInt();
+            System.out.println(i + "回目の入力した値は" + n + "です.");
+            System.out.println("直前に入力した値は" + nprev + "です.");
+            nprev = n;
+            i++;
+        }
+
+        stdIn.close();
+
+    }
+}
